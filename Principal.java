e800b1a51385aa59eb0df8454e3bdb4220a65f3e
danielgoorjr <82066159+danielgoorjr@users.noreply.github.com>
2021-05-02 19:12:42 -0300

    Alteração dos métodos na main
    Alteração dos métodos na main

--- Principal.java
+++ Principal.java
@@ -1,19 +1,17 @@
 public class Principal {
-  
+
   public static void main(String[] args) {
 
     // Retângulo
     Retangulo r = new Retangulo();
-    r.areaRet(5.5f, 6.4f);
-    r.perimetroRet(7.2f, 4.6f);
-
+    r.area(5.5f, 6.4f);
+    r.perimetro(7.2f, 4.6f);
 
     // Circunferencia
     Circunferencia c = new Circunferencia();
     c.areaCir(2.5f);
     c.perimetroCir(5.6f);
 
-    
   }
 
 }
