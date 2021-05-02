146dcd437610c3851602f684cc36bdb93df0afad
akiofabio <fabio.ikenoue@fatec.sp.gov.br>
2021-05-02 18:45:04 -0300

    Update Principal.java
    adicionado o teste da classe Retangulo

--- Principal.java
+++ Principal.java
@@ -3,4 +3,9 @@ public class Principal {
   public static void main(String[] args) {
   }
 
+    // Retângulo
+    Retangulo r = new Retangulo();
+    r.area(5.5f, 6.4f);
+    r.perimetro(7.2f, 4.6f);
+
 }
