f111df38403f4b21abb436dbffae51dae416d845
akiofabio <fabio.ikenoue@fatec.sp.gov.br>
2021-05-02 18:31:50 -0300

    Update Retangulo.java
    Adicionado o método do calculo a área

--- Retangulo.java
+++ Retangulo.java
@@ -3,4 +3,10 @@ public class Retangulo {
   float d1;
   float d2;
 
+  // Métodos
+
+  public void area(float d1, float d2) {
+    float a = d1 * d2;
+    System.out.println("A área do retângulo de lado " + d1 + " e " + d2 + " é: " + a);
+  }
 }
