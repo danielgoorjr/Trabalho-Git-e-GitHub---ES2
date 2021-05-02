8af9f62cc53e8ae9c65cecaa7342a036c5f03215
akiofabio <fabio.ikenoue@fatec.sp.gov.br>
2021-05-02 18:34:16 -0300

    Update Retangulo.java
    Adicionado o método do calculo do perímetro

--- Retangulo.java
+++ Retangulo.java
@@ -9,4 +9,10 @@ public class Retangulo {
     float a = d1 * d2;
     System.out.println("A área do retângulo de lado " + d1 + " e " + d2 + " é: " + a);
   }
+
+  public void perimetro(float d1, float d2) {
+    float p = 2 * (d1 + d2);
+    System.out.println("O perímetro do retângulo de lado " + d1 + " e " + d2 + " é: " + p);
+  }
+
 }
