f431c9807faa5cda7e7d663200dc726f9469a5d7
danielgoorjr <82066159+danielgoorjr@users.noreply.github.com>
2021-05-02 19:00:58 -0300

    Instanciando o retangulo

--- Principal.java
+++ Principal.java
@@ -2,6 +2,12 @@ public class Principal {
   
   public static void main(String[] args) {
 
+    // Retângulo
+    Retangulo r = new Retangulo();
+    r.areaRet(5.5f, 6.4f);
+    r.perimetroRet(7.2f, 4.6f);
+
+
     
   }
 
