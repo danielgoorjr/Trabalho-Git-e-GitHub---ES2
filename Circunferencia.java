0ad86b50b2396690e98353103f632e5f03ad8c39
danielgoorjr <82066159+danielgoorjr@users.noreply.github.com>
2021-05-02 18:56:45 -0300

    Update Circunferencia.java

--- Circunferencia.java
+++ Circunferencia.java
@@ -7,5 +7,7 @@ public class Circunferencia {
 
   public void areaCir(float d1){
     float a = (float) (Math.PI*d1*d1);
+    System.out.println("A área da circunferência de raio "+d1+" é: "+a);
+  }
 
 }
